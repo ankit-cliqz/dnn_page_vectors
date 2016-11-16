7c5ca0335426e5f22346d99d17241a80ca931e38
Ankit Bahuguna <ankit@cliqz.com>
2016-11-16 11:22:07 +0100

    fixed exception handling with the urls. in page info collector.

--- data_collection/page_info_collector.py
+++ data_collection/page_info_collector.py
@@ -11,7 +11,7 @@
 """
 
 import ujson as json
-import pykeyvi
+# import pykeyvi
 from cache.db.sm import SM
 from cache.db.word2vec_normalizer import Word2VecTextNormalizer
 from cache.db.data_utils import DataUtils
@@ -42,86 +42,87 @@ def get_top_queries_source(data_json, top_n_queries, source):
 
 
 url_set = set()
-with open("/raid/urls/all_urls_unique.txt", "r") as fr:
+with open("/ebs/ankit/all_urls_unique.txt", "r") as fr:
     for line in fr:
         line = json.loads(line)
         if not line.strip() == "" and line.startswith('http'):
             url_set.add(str(line.strip()))
 
-out_file = open("/raid/train_data_for_urls.txt", "w")
-keyvi_index_compiler = pykeyvi.JsonDictionaryCompiler()
+out_file = open("/raid/page_info_collected_urls.txt", "w")
+# keyvi_index_compiler = pykeyvi.JsonDictionaryCompiler()
 ucount = 0
 
 for url in url_set:
-    ucount+=1
-    if ucount % 10000 == 0:
-        print "Currently Processed: {}".format(ucount)
-    correct_url_found = False
-    # Handle the case for missing 'web-url' prefixes
-    url_tmp = url
-    #print "URL: {}".format(url)
-    #print "URL Type: {}".format(type(url))
-    data = sm.info_page(url_tmp)
-    #print data
-    #print "Data Type: {}".format(type(data))
-    if data['info'] and not data['info'] is None:
-        if not data['info'] == 'not in index':
-            correct_url_found = True
-
-
-    # Handle the case for missing training '/' in a web url for the cases only where the url was not found in the index
-    if correct_url_found == False:
-        #for prefix in url_prefix_list:
-        url_tmp = url + '/'
-        # print "URL: {}".format(url)
-        # print "URL Type: {}".format(type(url))
+    try:
+        ucount+=1
+        if ucount % 10000 == 0:
+            print "Currently Processed: {}".format(ucount)
+        correct_url_found = False
+        # Handle the case for missing 'web-url' prefixes
+        url_tmp = url
+        #print "URL: {}".format(url)
+        #print "URL Type: {}".format(type(url))
         data = sm.info_page(url_tmp)
-        # print "Data Type: {}".format(type(data))
+        #print data
+        #print "Data Type: {}".format(type(data))
         if data['info'] and not data['info'] is None:
             if not data['info'] == 'not in index':
                 correct_url_found = True
 
-    if correct_url_found == False:
-        nf+=1
-        print "URL NOT FOUND IN INDEX: {}".format(url)
-        continue
 
-    #print "yes"
-    data = sm.info_page(url_tmp)
-    #print data
-    if data['info'] and not data['info'] is None:
-        top_n_queries = []
+        # Handle the case for missing training '/' in a web url for the cases only
+        # where the url was not found in the index
+        if correct_url_found == False:
+            #for prefix in url_prefix_list:
+            url_tmp = url + '/'
+            # print "URL: {}".format(url)
+            # print "URL Type: {}".format(type(url))
+            data = sm.info_page(url_tmp)
+            # print "Data Type: {}".format(type(data))
+            if data['info'] and not data['info'] is None:
+                if not data['info'] == 'not in index':
+                    correct_url_found = True
 
-        # Collect top queries linked to page
-        if (not data['info'].get("tq") is None):
-            for s in sources:
-                get_top_queries_source(data['info'], top_n_queries, s)
+        if correct_url_found == False:
+            nf+=1
+            print "URL NOT FOUND IN INDEX: {}".format(url)
+            continue
 
-        url_id_data = ""
-        if data['url_id'] is not None:
-            url_id_data = data['url_id']
+        #print "yes"
+        data = sm.info_page(url_tmp)
+        #print data
+        if data['info'] and not data['info'] is None:
+            top_n_queries = []
+
+            # Collect top queries linked to page
+            if (not data['info'].get("tq") is None):
+                for s in sources:
+                    get_top_queries_source(data['info'], top_n_queries, s)
+
+            url_id_data = ""
+            if data['url_id'] is not None:
+                url_id_data = data['url_id']
 
 
-        title = ""
-        desc = ""
-
-        # print url_tmp
-        if data['snippets'] and not data['snippets'] == 'no snippet info':
-            if data['snippets']['snippet']:
-                    if data['snippets']['snippet'].get('title') is not None:
-                        title = data['snippets']['snippet']['title']
-        else:
             title = ""
-
-        if data['snippets'] and not data['snippets'] == 'no snippet info' :
-            if data['snippets']['snippet']:
-                    if data['snippets']['snippet'].get('desc') is not None:
-                        desc = data['snippets']['snippet']['desc']
-        else:
             desc = ""
 
-        url_words = " ".join(du.words_from_url(url_tmp, order=True))
-        try:
+            # print url_tmp
+            if data['snippets'] and not data['snippets'] == 'no snippet info':
+                if data['snippets']['snippet']:
+                        if data['snippets']['snippet'].get('title') is not None:
+                            title = data['snippets']['snippet']['title']
+            else:
+                title = ""
+
+            if data['snippets'] and not data['snippets'] == 'no snippet info' :
+                if data['snippets']['snippet']:
+                        if data['snippets']['snippet'].get('desc') is not None:
+                            desc = data['snippets']['snippet']['desc']
+            else:
+                desc = ""
+
+            url_words = " ".join([wn.preprocess_line(du.extract_domain(url_tmp))] + du.words_from_url(url_tmp, ordered_list=True))
             if not len(top_n_queries) == 0:
                 data_out = {
                     'url_words': url_words,
@@ -140,16 +141,16 @@ for url in url_set:
                 # print data_out
 
             out_file.write("{}\t{}\n".format(json.dumps(url_tmp), json.dumps(data_out)))
-            keyvi_index_compiler.Add(str(url_tmp), json.dumps(data_out))
-        except Exception as e:
-            print "Exception encountered for url: {}, exception: {}".format(url_tmp, e)
+            #keyvi_index_compiler.Add(str(url_tmp), json.dumps(data_out))
+    except Exception as e:
+        print "Exception encountered for url: {}, exception: {}".format(url, e)
 
 out_file.close()
 print "Number of Total URLS: {}".format(len(url_set))
 print "Number of url-ids mis-match URLS: {}".format(uc)
 print "Number of URLS not found in index: {}".format(nf)
-print "\nCompiling Keyvi Index < url:id - url for training data >... "
-keyvi_index_compiler.Compile()
-print "Finished Compiling Keyvi. Now writing keyvi index to file... "
-keyvi_index_compiler.WriteToFile('/raid/train_data_for_urls.kv')
+# print "\nCompiling Keyvi Index < url:id - url for training data >... "
+# keyvi_index_compiler.Compile()
+# print "Finished Compiling Keyvi. Now writing keyvi index to file... "
+# keyvi_index_compiler.WriteToFile('/raid/train_data_for_urls.kv')
 print "Finished writing keyvi file!"
